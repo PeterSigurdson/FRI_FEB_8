4b63c189d821fd21f1e7b0569d6a8fa977265f60
peter sigurdson <peter@petersigurdson.net>
2019-02-08 20:31:06 -0500

    8:30 pm commit

--- FRI_FEB_8/Program.cs
+++ FRI_FEB_8/Program.cs
@@ -15,10 +15,15 @@ namespace FRI_FEB_8
     class Node
     {
         public Node() { }
-        public Node floor;
         public Node elevatorUp;
         public string FloorNumber;
     }  
+
+    class Department
+    {
+        public Department aDepartment;
+
+    }
     
     class Elevator
     {
@@ -51,13 +56,12 @@ namespace FRI_FEB_8
             temp = Head;
          
             // where am I going to start?
-            Console.WriteLine("The first floor is " + Head.FloorNumber);
+            
             while (temp != null)
             {
-                // NOW GET TO THE SECOND FLOOR!!!!
-                temp = temp.elevatorUp;
                 Console.WriteLine(temp.FloorNumber);
-                    
+                temp = temp.elevatorUp;
+                
             }
 
         }
